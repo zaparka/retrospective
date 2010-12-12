ef00ecc2af54274eab8f4742b695968c3f17d479
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-12-12 16:36:51 +0000

    Rails 3.0.3 version.

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'database_cleaner'
+  gem 'cucumber-rails'
+  gem 'cucumber'
+  gem 'capybara'
+  gem 'rspec-rails'
+  gem 'launchy'    # So you can do Then show me the page
+  gem 'pickle'
+  gem 'factory_girl_rails'
+  gem 'ruby-debug'
+  gem 'heroku'
+  gem 'rcov'
+  gem 'annotate'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retrospective::Application.config.secret_token = '4df9a55e82b550218d0da63690dc7644e10e7bb5acbfdd8ecfbf87e59006405bca856c566a307f66800ab107c80f1fab8fc534cf22aec4c25971ef1bc0587ecd'
