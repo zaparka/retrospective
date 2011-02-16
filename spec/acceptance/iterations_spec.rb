1e5073a989cea2347074f91c2c99bc735fd5f8e5
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2011-02-16 15:56:22 +0000

    Steak for iterations.

--- spec/acceptance/iterations_spec.rb
+++ spec/acceptance/iterations_spec.rb
@@ -1,12 +1,52 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 feature "Iterations", %q{
-  In order to ...
-  As a ...
-  I want to ...
+  In order to add iteration for project
+  as a user
+  I want CRUD functionality
 } do
 
-  scenario "Scenario name" do
-    true.should == true
+  scenario "Register new iteration" do
+    project = Factory(:project, :name => "Retrospective")
+    visit project_url(project)
+
+    page.should have_content('Retrospective')
+    click_link "New Iteration"
+    current_url.should == new_project_iteration_url(project)
+
+    fill_in "Name", :with => "IT 1"
+
+    select "2011", :from => "iteration_start_1i"
+    select "February", :from => "iteration_start_2i"
+    select "5", :from => "iteration_start_3i"
+
+    select "2011", :from => "iteration_end_1i"
+    select "February", :from => "iteration_end_2i"
+    select "25", :from => "iteration_end_3i"
+
+    click_button "Create"
+    page.should have_content('Iteration was successfully created.')
+    current_url.should == project_iteration_url(project, Iteration.find_by_name("IT 1"))
+  end
+
+  scenario "Delete iteration" do
+    project = Factory(:project, :name => "Thankerino")
+    it1 = Factory(:iteration, :name => "it1", :project => project)
+    it2 = Factory(:iteration, :name => "it2", :project => project)
+    it3 = Factory(:iteration, :name => "it3", :project => project)
+    it4 = Factory(:iteration, :name => "it4", :project => project)
+
+    visit project_url(project)
+    page.has_xpath?('.//table/tbody/tr[1]/td', :text => "it1")
+    page.has_xpath?('.//table/tbody/tr[2]/td', :text => "it2")
+    page.has_xpath?('.//table/tbody/tr[3]/td', :text => "it3")
+    page.has_xpath?('.//table/tbody/tr[4]/td', :text => "it4")
+    find(:xpath, './/table/tr[3]/td/a', :text => "Destroy").click
+    current_url.should == project_url(project)
+
+    page.has_xpath?('.//table/tbody/tr[1]/td', :text => "it1")
+    page.has_xpath?('.//table/tbody/tr[2]/td', :text => "it2")
+    page.has_no_xpath?('.//table/tbody/tr[3]/td', :text => "it3")
+    page.has_xpath?('.//table/tbody/tr[4]/td', :text => "it4")
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,20 @@
 require 'factory_girl'
 
 Factory.define :project do |p|
-  p.name  "Retrospective"
+  p.name { Factory.next(:project_name) }
+end
+
+Factory.define :iteration do |p|
+  p.name { Factory.next(:iteration_name) }
+  p.project {|a| a.association(:project) }
+  p.start 2.day.ago
+  p.end 2.days.from_now
+end
+
+Factory.sequence :iteration_name do |n|
+  "Category #{n}"
+end
+
+Factory.sequence :project_name do |n|
+  "Retrospective #{n}"
 end
